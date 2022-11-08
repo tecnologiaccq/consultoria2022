a6f16b7834b9cc28168da23b7f4a4b8405c0c65b
Luis Guairacaja Soque <laguairacaja@lacamaradequito.com>
2022-11-08 15:57:18 -0500

    script emisión de notas de crédito, LGUAIRACAJA, 08/nov/2022

--- LuisGuairacaja/script_002.sql
+++ LuisGuairacaja/script_002.sql
@@ -54,7 +54,9 @@ insert into tGN_TiposEmisionCuotaCCQ (IdTipoEmisionCuotaCCQ, Codigo,Nombre) valu
 --ID: 6	
 -- Agregar nuevo campo en tabla Socio que permita identificar si la emision es de tipo ordinaria o anticipada 
 ALTER TABLE tGS_Socios add IdTipoEmisionCuotaCCQ  TINYINT DEFAULT(1) --[1] Ordinaria, [2] Anticipada
-ALTER TABLE tGS_Socios add IdMesEmisionCuotaAnticipada TINYINT DEFAULT(1) --[1]Enero, [2] Febrero, ...[12]Diciembre
+-- eliminar esta campo
+-- ALTER TABLE tGS_Socios add IdMesEmisionCuotaAnticipada TINYINT DEFAULT(1) --[1]Enero, [2] Febrero, ...[12]Diciembre
+
 ALTER TABLE tGS_Socios add ExcluidoParaEmision  bit DEFAULT(0) --  True/1  Si el socio esta excluido,no se lo va a emitir 
 ALTER TABLE tGS_Socios add IdMesEmisionBimestral  TINYINT DEFAULT(1) --[1] MesImpar, [2]  MesPar, para cuotas ordinarias
 
@@ -62,170 +64,43 @@ ALTER TABLE [dbo].[tGS_Socios]  WITH CHECK ADD  CONSTRAINT [FK_tGS_Socios_TipoEm
 REFERENCES [dbo].[tGN_TiposEmisionCuotaCCQ] ([IdTipoEmisionCuotaCCQ])
 GO
 
-
-/****** Object:  Table [dbo].[tGS_Cuotas]    Script Date: 21/10/2022 15:24:44 ******/
-SET ANSI_NULLS ON
-GO
-
 -- drop table tGS_Cuotas_V2
 CREATE TABLE [dbo].[tGS_Cuotas_V2](
-	[IdCuota] [int] IDENTITY(1,1) NOT NULL,
+	[IdCuota] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[IdSocio] [int] NOT NULL,
-	[IdTipoCuota] [int] NULL,
-	[IdFactura] [int] NULL,
-	[IdNotaCredito] [int] NULL,
-	[IdMedioPago] [int] NULL,
-	[IdCobrador] [int] NULL,
+	[IdCobrador] [int] NOT NULL,
 	[IdVendedor] [int] NULL,
-	[IdFrecuenciaGeneracionCuota] [int] NULL,
-	[IdPeriodo] [int] NULL,
-	[NroMeses] [int] NULL,
-	[FechaCuota] [date] NULL,
-	[FechaVencimiento] [date] NULL,
-	[ValorBruto] [decimal](18, 5) NULL,
-	[ValorCuota] [decimal](18, 5) NULL,
-	[ValorDescuento] [decimal](18, 5) NULL,
-	[Saldo] [decimal](18, 5) NULL,
-	[SaldoCuotaAnticipada] [decimal](18, 5) NULL,
-	[ValorCuotaDevengar] [decimal](18, 5) NULL,
-	[ValorDescuentoDevengar] [decimal](18, 5) NULL,
-	[Descripcion] [nvarchar](128) NULL,
-	[Anulada] [bit] NULL,
-	[FacturaGenerada] [bit] NULL,
-	[NCGenerada] [bit] NULL,
-	[IdCuotaOriginal] [int] NULL,
-	[EsCuotaDevengada] [bit] NULL,
-	[FechaCondonacion] [datetime] NULL,
-	[ObservacionCondonacion] [nvarchar](256) NULL,
-	[CreadoPor] [nvarchar](32) NULL,
-	[FechaCreacion] [datetime] NULL,
-	[EstacionCreacion] [nvarchar](24) NULL,
-	[ModificadoPor] [nvarchar](32) NULL,
-	[UltimaModificacion] [datetime] NULL,
-	[EstacionModificacion] [nvarchar](24) NULL,
-	[InformacionAdicionalFactura] [nvarchar](300) NULL,
-	[IdAsientoContable] [int] NULL,
-	[EsCuotaManual] [bit] NULL,
+	[NroMeses] [int] NOT NULL, --Número de meses que contiene la cuota
+	[FechaCuota] [date] NOT NULL,  
+	FechaEmision DATE NOT null , 
+	[FechaVencimiento] [date] NOT null ,
+	[ValorCuota] [decimal](18, 2) not NULL check(ValorCuota>=0),
+	[ValorDescuento] [decimal](18, 2) not NULL, check(ValorDescuento>=0),
+	ValorPorcentajeIva decimal(18,2) NOT NULL CHECK(ValorPorcentajeIva>=0), -- sera en 0.12, 0.00
+	ValorIva DECIMAL(18,2) not NULL check(ValorIva>=0),
+	ValorTotal DECIMAL(18,2) not NULL check(ValorTotal>=0),
+	IdMesEmisionBimestral  TINYINT NOT NULL, --[1] MesImpar, [2]  MesPar, para cuotas ordinarias
+	[Descripcion] [nvarchar](128) NOT NULL,
+	[CreadoPor] [nvarchar](32) NOT NULL,
+	[FechaCreacion] [datetime] NOT NULL,
+	[EstacionCreacion] [nvarchar](24) NOT NULL,
+	[ModificadoPor] [nvarchar](32) NOT NULL,
+	[UltimaModificacion] [datetime] NOT NULL,
+	[EstacionModificacion] [nvarchar](24) NOT NULL,
 
-	IdTipoEmisionCuotaCCQ  TINYINT DEFAULT(1), --[1] Ordinaria, [2] Anticipada
-	IdMesEmisionBimestral  TINYINT DEFAULT(1), --[1] MesImpar, [2]  MesPar, para cuotas ordinarias
-	MesInicial DATE, --
-	MesFinal DATE, --
-	FechaEmision DATE, 
-	Valor DECIMAL(18,2),
-	ValorIVA DECIMAL(18,2),
-	ValorTOTAL DECIMAL(18,2)
+	CHECK (ValorIVA = (ValorCuota - ValorDescuento) * ValorPorcentajeIva),
+	CHECK (ValorTotal = ValorIVA  +  ValorCuota - ValorDescuento)
+ )
 
+ -- REVISAR ESTA RELACION 
+ 
+--ALTER TABLE tCC_CargosDet WITH CHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_tCC_CargosDet] FOREIGN KEY([IdCuota])
+--REFERENCES [dbo].[tGS_Cuotas_V2] ([IdCuota])
 
- CONSTRAINT [PK_tGS_Cuotas_V2] PRIMARY KEY CLUSTERED 
-(
-	[IdCuota] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+ALTER TABLE tCC_DescargosDet Add IdCuota INT
+--ALTER TABLE tCC_DescargosDet WITH CHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_tCC_DescargosDet] FOREIGN KEY([IdCuota])
+--REFERENCES [dbo].[tGS_Cuotas_V2] ([IdCuota])
 
-ALTER TABLE [dbo].tGS_Cuotas_V2 ADD  CONSTRAINT [DF_tGS_Cuotas_V2_ValorDescuento]  DEFAULT ((0)) FOR [ValorDescuento]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 ADD  CONSTRAINT [DF_tGS_Cuotas_V2_ValorDescuentoDevengar]  DEFAULT ((0)) FOR [ValorDescuentoDevengar]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 ADD  CONSTRAINT [DF__tmp_ms_xx__Anula__7DA9E828_V2]  DEFAULT ((0)) FOR [Anulada]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 ADD  CONSTRAINT [DF__tmp_ms_xx__Factu__7E9E0C61_V2]  DEFAULT ((0)) FOR [FacturaGenerada]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 ADD  CONSTRAINT [DF__tmp_ms_xx__NCGen__7F92309A_V2]  DEFAULT ((0)) FOR [NCGenerada]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 ADD  CONSTRAINT [DF__tmp_ms_xx__EsCuo__008654D3_V2]  DEFAULT ((0)) FOR [EsCuotaDevengada]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 ADD  CONSTRAINT [DF__tGS_Cuota_V2__EsCuo__1F4E0071]  DEFAULT ((0)) FOR [EsCuotaManual]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2  WITH CHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_Cobradores] FOREIGN KEY([IdCobrador])
-REFERENCES [dbo].[tCC_Cobradores] ([IdCobrador])
-GO
-ALTER TABLE [dbo].tGS_Cuotas_V2 CHECK CONSTRAINT [FK_tGS_Cuotas_V2_Cobradores]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2  WITH NOCHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_IdFrecuenciaGeneracionCuota] FOREIGN KEY([IdFrecuenciaGeneracionCuota])
-REFERENCES [dbo].[tGS_FrecuenciaGeneracionCuotas] ([IdFrecuenciaGeneracionCuota])
-NOT FOR REPLICATION 
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 CHECK CONSTRAINT [FK_tGS_Cuotas_V2_IdFrecuenciaGeneracionCuota]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2  WITH NOCHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_IdMedioPago] FOREIGN KEY([IdMedioPago])
-REFERENCES [dbo].[tGN_MediosPagos] ([IdMedioPago])
-NOT FOR REPLICATION 
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 CHECK CONSTRAINT [FK_tGS_Cuotas_V2_IdMedioPago]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2  WITH NOCHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_IdTipoCuota] FOREIGN KEY([IdTipoCuota])
-REFERENCES [dbo].[tCC_TiposDocumentosCCQ] ([IdTipoDocumentoCCQ])
-NOT FOR REPLICATION 
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 CHECK CONSTRAINT [FK_tGS_Cuotas_V2_IdTipoCuota]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2  WITH CHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_NotasCredito] FOREIGN KEY([IdNotaCredito])
-REFERENCES [dbo].[tCC_DescargosCab] ([IdDescargoCab])
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 CHECK CONSTRAINT [FK_tGS_Cuotas_V2_NotasCredito]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2  WITH CHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_Periodo] FOREIGN KEY([IdPeriodo])
-REFERENCES [dbo].[tCG_PeriodoContable] ([IdPeriodoContable])
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 CHECK CONSTRAINT [FK_tGS_Cuotas_V2_Periodo]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2  WITH CHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_tGS_Socios] FOREIGN KEY([IdSocio])
-REFERENCES [dbo].[tGS_Socios] ([IdSocio])
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2 CHECK CONSTRAINT [FK_tGS_Cuotas_V2_tGS_Socios]
-GO
-
-ALTER TABLE [dbo].tGS_Cuotas_V2  WITH CHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_Vendedores] FOREIGN KEY([IdVendedor])
-REFERENCES [dbo].[tCC_Vendedores] ([IdVendedor])
-GO
 
 GO
-
-
-
-
-ALTER TABLE tCC_DescargosDet add IdCuota int
-ALTER TABLE tCC_DescargosDet WITH CHECK ADD  CONSTRAINT [FK_tGS_Cuotas_V2_tCC_DescargosDet] FOREIGN KEY([IdCuota])
-REFERENCES [dbo].[tGS_Cuotas_V2] ([IdCuota])
-GO
-
--- 
-
-
-
----- UPDATE DE REGISTROS
---update tGS_Socios 
---set ExcluidoParaEmision =  0 -- FALSE, para los socios activos
---where IdEstadoSocio = 1
---go
---update tGS_Socios 
---set ExcluidoParaEmision =  1 -- TRUE, para resto de registros
---where IdEstadoSocio is null
-
-
-
-
---*/
-
 
---
+++ LuisGuairacaja/script_EMisionNotasCredito.sql
@@ -0,0 +1,376 @@
+USE [CCQ_DESA]
+
+/* 
+Se agrega un nuevo campo [IdCargoCab] a la tabla de Descargo.
+Permitirá un cruce más rápido de los detalles
+*/
+alter table tCC_DescargosCab add IdCargoCab int   --*** DESCOMENTAR, antes de pasar a AMBIENTE
+
+-- La Columna [NumeroDocumento] es la concatenacion de los campos Establecimiento-PuntoEmision-Secuencial
+ALTER TABLE dbo.tCC_DescargosCab DROP COLUMN NumeroDocumento;
+GO
+ALTER TABLE tCC_DescargosCab ADD NumeroDocumento  AS (concat([Establecimiento],'-',[PuntoEmision],'-',[Secuencial]))
+
+
+GO
+DROP TRIGGER [tCC_DescargosCabAIIdCobrador]
+GO
+
+GO
+ALTER TRIGGER [dbo].[tCC_DescargosCabAIU]
+ON [dbo].[tCC_DescargosCab]
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    UPDATE dbo.tCC_DescargosCab
+    SET UltimaModificacion = CURRENT_TIMESTAMP,
+        ModificadoPor = SUSER_NAME(),
+        EstacionModificacion = HOST_NAME(),
+        CreadoPor = IIF(t.CreadoPor IS NULL, SUSER_NAME(), t.CreadoPor),
+        FechaCreacion = IIF(t.FechaCreacion IS NULL, GETDATE(), t.FechaCreacion),
+        EstacionCreacion = IIF(t.EstacionCreacion IS NULL, HOST_NAME(), t.EstacionCreacion)
+
+    FROM dbo.tCC_DescargosCab AS t
+        INNER JOIN INSERTED AS i
+            ON t.IdDescargoCab = i.IdDescargoCab;
+END;
+GO
+
+USE [CCQ_DESA]
+GO
+/****** Object:  Trigger [dbo].[tCC_DescargosCabExtensionAI]    Script Date: 8/11/2022 14:03:40 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER TRIGGER [dbo].[tCC_DescargosCabExtensionAI]
+ON [dbo].[tCC_DescargosCab]
+   AFTER INSERT
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @IdDescargoCab INT = 0;
+	SELECT @IdDescargoCab = IdDescargoCab FROM Inserted;
+	
+	INSERT INTO dbo.tCC_DescargosCabExtension
+	  (
+	    IdDescargoCab,
+	    RazonSocial,
+	    Identificacion,
+	    IdTipoIdentifcacion,
+	    CorreoElectronico,
+	    Telefono,
+	    Direccion,
+	    IdAsesor,
+	    IdParroquia,
+	    ReferenciaDireccion,
+		IdEmpresa
+	  )
+
+SELECT i.IdDescargoCab, cli.RazonSocial, cli.Identificacion, cli.IdTipoIdentificacion,
+stuff((
+        select ';' + t.Contacto
+        from tGN_ContactoPersona t
+        where t.IdPersona = per.IdPersonas 
+		AND t.IdMedioContacto = 15
+        for xml path('')
+    ),1,1,'') as CorreoElectronico
+	,MAX(cto_telef.Contacto) AS Telefono
+, MAX(dir.DireccionCompleta)AS Direccion
+,socio.IdAsesor
+, MAX(dir.IdParroquia) AS IdParroquia
+, MAX(dir.Referencia) AS Referencia
+, i.IdEmpresa
+from  Inserted i
+INNER JOIN tCC_Clientes cli on (cli.IdCliente =  i.IdCliente)
+INNER JOIN tGN_Personas per on (per.IdPersonas = cli.IdPersona) 
+LEFT JOIN tGS_Socios socio on (socio.IdSocio = cli.IdSocio)
+LEFT JOIN tGN_ContactoPersona cto_telef on (cto_telef.IdPersona = per.IdPersonas AND cto_telef.IdMedioContacto = 6  and cto_telef.Principal = 1)
+LEFT JOIN tGN_Direcciones dir on (dir.IdPersona = per.IdPersonas AND dir.IdTipoDireccion = 1 and dir.Principal = 1)
+AND NOT EXISTS (
+	               SELECT idcargocab
+	               FROM   tCC_CargosCabExtension
+	               WHERE  idcargocab = i.IdDescargoCab)
+GROUP BY i.IdDescargoCab, cli.RazonSocial, cli.Identificacion, cli.IdTipoIdentificacion,socio.IdAsesor,per.IdPersonas,i.IdEmpresa 
+END
+
+GO
+
+
+
+/*
+--============================================================================================================
+-- Emision de notas de crédito completas
+*/
+CREATE OR ALTER PROC sp_CC_EmitirNotaCreditoCompleta_V2
+(
+@ArregloIdCargoCab ArregloIdCargoCab READONLY, 
+@Motivo  VARCHAR(300)
+)
+AS
+
+--DECLARE @ArregloIdCargoCab ArregloIdCargoCab
+--DECLARE @Motivo  VARCHAR(300) = 'ANULACIÓN TOTAL DE LA FACTURA 001-019-000126808'
+--INSERT INTO @ArregloIdCargoCab values(291511);
+
+--En esta variable se retorna el id del Asiento generado o en caso del error el texto de mismo  
+DECLARE @IdAsientoContableCab VARCHAR(2000) = NULL 
+DECLARE @IdEmpresa INT = 0
+DECLARE @IdTipoDocumento INT = 0
+DECLARE @IdCaja INT = 0
+DECLARE @Establecimiento CHAR(3) = ''
+DECLARE @PuntoEmision CHAR(3) = ''
+DECLARE @IdPuntoEmision INT = 0
+DECLARE @IdTipoDocumentoCCQ INT = 0
+DECLARE @IdTipoCobro INT = 0
+DECLARE @IdPeriodoContable INT = 0
+DECLARE @SecuencialInt INT = 0    
+DECLARE @Secuencial CHAR(9) = ''   
+DECLARE @IdEstadoDocumento INT = 0
+DECLARE @IdGrupoComprobante INT = 0
+DECLARE @NumeroComprobanteActual INT = 0
+DECLARE @IdDescargoCabInserted INT = 0
+
+
+SELECT @IdEmpresa = cargo.IdEmpresa , @IdTipoDocumentoCCQ = cargo.IdTipoDocumentoCCQ
+FROM tCC_CargosCab cargo
+INNER JOIN @ArregloIdCargoCab arrIds on cargo.IdCargoCab = arrIds.IdCargoCab;
+
+select @IdTipoDocumento = IdTipoDocumento FROM tGN_TiposDocumentos WHERE CodigoDocumentoSRI = '04' AND IdEmpresa = @IdEmpresa
+SELECT @IdCaja = Valor FROM tGN_ParametrosGenerales WHERE Codigo = 'ID_CAJA_NOTACREDITO' and IdEmpresa = @IdEmpresa
+SELECT @IdPuntoEmision = IdPuntoEmision FROM tCC_Caja WHERE IdCaja = @IdCaja
+SELECT @IdTipoCobro = IdTipoCobro FROM tCC_TiposCobros WHERE IdEmpresa = @IdEmpresa and Codigo = 'CAJ'
+SELECT @Establecimiento = Establecimiento FROM tGN_Establecimientos estab WHERE estab.IdEmpresa = @IdEmpresa
+SELECT @PuntoEmision = PuntoEmision FROM tGN_PuntosEmision WHERE IdPuntoEmision = @IdPuntoEmision
+select @IdPeriodoContable = IdPeriodoContable FROM tCG_PeriodoContable WHERE IdEstadoPeriodoContable = 1 and IdEmpresa = @IdEmpresa
+select @IdGrupoComprobante = IdGrupoComprobante from tCG_GrupoComprobantes WHERE IdEmpresa = @IdEmpresa and Codigo = 'DIA'
+select @IdEstadoDocumento = IdEstadoDocumento from tCT_EstadoDocumentoSRI WHERE Codigo = 'CARGA'
+
+-- Get el secuencial disponible para la NC
+SELECT @SecuencialInt = Secuencial  
+FROM   tGN_Secuenciales WITH (XLOCK, TABLOCK)  
+WHERE  IdPuntoEmision = @IdPuntoEmision  
+AND IdTipoDocumento = @IdTipoDocumento;      
+
+SET @Secuencial = [dbo].[fn_LeftPAD](CONVERT(VARCHAR, @SecuencialInt), 9, '0');  
+
+-- get el NumeroComprobante Actual
+SELECT @NumeroComprobanteActual = dbo.ObtenerNumeroComprobanteActual(  @IdEmpresa, @IdPeriodoContable, @IdGrupoComprobante)
+
+PRINT '@@IdEmpresa: ' + convert(varchar,@IdEmpresa)
+PRINT '@IdTipoDocumento: ' + convert(varchar,@IdTipoDocumento)
+PRINT '@@IdTipoDocumentoCCQ: ' + convert(varchar,@IdTipoDocumentoCCQ)
+PRINT '@IdCaja: ' + convert(varchar,@IdCaja)
+PRINT '@IdPuntoEmision: ' + convert(varchar,@IdPuntoEmision)
+PRINT '@IdTipoCobro: ' + convert(varchar,@IdTipoCobro)
+PRINT '@Establecimiento: ' + convert(varchar,@Establecimiento)
+PRINT '@PuntoEmision: ' + convert(varchar,@PuntoEmision)
+PRINT '@IdPeriodoContable: ' + convert(varchar,@IdPeriodoContable)
+PRINT '@Secuencial: ' + @Secuencial
+PRINT '@NumeroComprobanteActual: ' + CONVERT(VARCHAR, @NumeroComprobanteActual)
+
+-- ------------------------------
+-- Validación de Errores
+DECLARE @resultadosValidacion VARCHAR(2000)  
+
+--Tratamiento de errores  
+ --====================================  
+ DECLARE @MENSAJE_ERROR NVARCHAR(4000)  
+ DECLARE @NUMERO_ERROR INT  
+ DECLARE @ERROR_SEVERITY INT  
+ DECLARE @ERROR_STATE INT  
+ --====================================  
+ 
+
+BEGIN TRY  
+  BEGIN TRANSACTION CC_Registrar_NotaCredito;  
+
+--[1]  Insert de la Cabecera del descargo
+
+INSERT INTO tCC_DescargosCab (
+IdCliente
+,IdTipoDocumento
+,IdEmpresa
+,IdPeriodoContable
+,Establecimiento
+,PuntoEmision
+,Secuencial
+,FechaEmision
+,SubtotalIva
+,Subtotal0
+,SubtotalNoObjetoImpuesto
+,TotalExcentoIva
+,SubtotalSinImpuesto
+,TotalDescuento
+,Iva
+,Ice
+,Irbpnr
+,ValorTotal
+,Saldo
+,Motivo
+,InformacionAdicional
+,IdEstadoDocumento
+,IdGrupoComprobante
+,NumeroComprobante
+,Concepto
+,IdTipoCobro
+,IdCaja
+,EnviarAlDoce
+,IdTipoDocumentoModificado
+,NumeroDocumentoModificado
+,FechaEmisionDocSustento
+,IdCobrador
+,IdCargoCab
+)
+select 
+cargo.IdCliente
+,@IdTipoDocumento as IdTipoDocumento
+,cargo.IdEmpresa as IdEmpresa 
+,@IdPeriodoContable as IdPeriodoContable
+,@Establecimiento as Establecimiento
+,@PuntoEmision as PuntoEmision
+,@Secuencial AS Secuencial
+,GETDATE() as FechaEmision
+,cargo.SubtotalIva
+,cargo.Subtotal0
+,cargo.SubtotalNoObjetoImpuesto
+,cargo.TotalExcentoIva
+,cargo.SubtotalSinImpuesto
+,cargo.TotalDescuento
+,cargo.Iva
+,cargo.Ice
+,cargo.Irbpnr
+,cargo.ValorTotal
+,cargo.Saldo
+,@Motivo
+,cargo.InformacionAdicional
+,@IdEstadoDocumento as IdEstadoDocumento
+,@IdGrupoComprobante as IdGrupoComprobante
+,@NumeroComprobanteActual as NumeroComprobante
+,@Motivo
+,@IdTipoCobro 
+,@IdCaja
+,cargo.EnviarAlDoce
+,cargo.IdTipoDocumento as IdTipoDocumentoModificado
+,cargo.NumeroDocumento as NumeroDocumentoModificado
+,cargo.FechaEmision as FechaEmisionDocSustento
+,cargo.IdCobrador
+,cargo.IdCargoCab
+from tCC_CargosCab cargo
+inner join @ArregloIdCargoCab arr on cargo.IdCargoCab = arr.IdCargoCab
+
+SET @IdDescargoCabInserted = 	CAST(IDENT_CURRENT('tCC_DescargosCab')  AS INT)-- Obtenemos el Id Ingresado en tCC_DescargosCab
+
+-- insert de detalles del descargo
+INSERT INTO tCC_DescargosDet(
+IdDescargoCab
+,CodigoPrincipal
+,CodigoAuxiliar
+,Descripcion
+,Cantidad
+,PrecioUnitario
+,Descuento
+,IdTipoImpuestoIva
+,ValorIva
+,IdTipoImpuestoIce
+,ValorIce
+,IdTipoImpuestoIrbpnr
+,ValorIrbpnr
+,Valor
+,IdPlanCuentasEmpresaDebito
+,IdPlanCuentasEmpresaCredito
+,IdCentroCosto
+,IdObjetoConsumo
+--,Debito
+--,Credito
+,IdCargoCab
+-- ,idnotacredito
+,IdCuota
+)
+select 
+@IdDescargoCabInserted--cab.IdDescargoCab
+,det.CodigoPrincipal
+,det.CodigoAuxiliar
+,det.Descripcion
+,det.Cantidad
+,det.PrecioUnitario
+,det.Descuento
+,det.IdTipoImpuestoIva
+,det.ValorIva
+,det.IdTipoImpuestoIce
+,det.ValorIce
+,det.IdTipoImpuestoIrbpnr
+,det.ValorIrbpnr
+,det.Valor
+--reemplazar el getdate por el primer dia dle periodo
+--,CASE WHEN det.IdPlanCuentasEmpresaCredito = 272 AND cuota.FechaCuota <= GETDATE() THEN det.IdPlanCuentasEmpresaCredito ELSE 327 END IdPlanCuentasEmpresaDebito
+,det.IdPlanCuentasEmpresaDebito IdPlanCuentasEmpresaDebito
+,det.IdPlanCuentasEmpresaDebito IdPlanCuentasEmpresaCredito
+,det.IdCentroCosto
+,det.IdObjetoConsumo
+,arr.IdCargoCab
+,det.IdCuota
+from tCC_CargosDet det
+inner join tCC_DescargosCab cab on det.IdCargoCab = cab.IdCargoCab
+inner join @ArregloIdCargoCab arr on cab.IdCargoCab = arr.IdCargoCab
+--inner join tGS_Cuotas_V2 cuota on det.IdCuota = cuota.IdCuota
+
+/*
+CRUCE DE DOCUMENTOS
+*/
+
+INSERT INTO tCC_TemporalCruceDoc
+(
+IdCliente,
+IdTipoDocumento,
+IdDescargoCab,
+Valor,
+Saldo,
+IdEstadoDocumento,
+IdCargoCab
+)
+select descargoCab.IdCliente, descargoCab.IdTipoDocumento, descargoCab.IdDescargoCab, descargoCab.ValorTotal, descargoCab.Saldo, descargoCab.IdEstadoDocumento, IdCargoCab 
+from tCC_DescargosCab descargoCab
+where IdDescargoCab = @IdDescargoCabInserted
+and descargoCab.IdTipoDocumento <> 59--Ingreso al fideicomiso
+
+-- --------------------------------------------
+-- Actualización de secuenciales
+-- --------------------------------------------
+UPDATE tGN_Secuenciales  
+SET    Secuencial = [dbo].[fn_LeftPAD](CONVERT(VARCHAR, @SecuencialInt + 1), 9, '0')  
+WHERE  IdPuntoEmision = @IdPuntoEmision  AND IdTipoDocumento = @IdTipoDocumento; 
+
+	-- si todo salió bien se ejecuta el commit    
+	--COMMIT TRANSACTION CC_Registrar_NotaCredito;     
+END TRY  
+ BEGIN CATCH  
+  SELECT @MENSAJE_ERROR = ERROR_MESSAGE(),  
+         @NUMERO_ERROR       = ERROR_NUMBER(),  
+         @ERROR_SEVERITY     = ERROR_SEVERITY(),  
+         @ERROR_STATE        = ERROR_STATE();  
+    
+  SET @IdAsientoContableCab = @MENSAJE_ERROR;  
+    
+  RAISERROR (@MENSAJE_ERROR, @ERROR_SEVERITY, @ERROR_STATE);  
+  IF EXISTS (  
+         SELECT [name]  
+         FROM   sys.dm_tran_active_transactions  
+         WHERE  NAME = 'CC_Registrar_NotaCredito'  
+     )  
+      ROLLBACK TRANSACTION CC_Registrar_NotaCredito;  
+ END CATCH;   
+ COMMIT TRANSACTION CC_Registrar_NotaCredito;  
+ RETURN  
+ERROR:  
+ SET @IdAsientoContableCab = @resultadosValidacion;  
+ RAISERROR (@resultadosValidacion, 16, 1);  
+ IF EXISTS (  
+        SELECT [name]  
+        FROM   sys.dm_tran_active_transactions  
+        WHERE  NAME = 'CC_Registrar_NotaCredito'  
+    )  
+     ROLLBACK TRANSACTION CC_Registrar_NotaCredito;  
+
+
